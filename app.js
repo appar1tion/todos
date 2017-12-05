b67157aee6bf33b53f621c72d9c06533a663ab9b
zabreezy <garbage1236@yahoo.com>
2017-12-04 20:25:36 -0800

    added browserify

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var sassMiddleware = require('node-sass-middleware');
+var browserify = require('browserify-middleware');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -23,6 +24,9 @@ app.use (
     debug: true,
   })
 );
+
+// browserify
+app.get('/javascripts/bundle.js', browserify('./client/script.js'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
